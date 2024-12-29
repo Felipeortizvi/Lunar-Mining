d508e386864c66c5ec8af1e413ed15408e693762
Felipe <felipeortizv19@gmail.com>
2024-12-29 18:27:51 -0500

    Deleted build files

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/felip/Desktop/CodeProjects/Lunar-Mining
-# Build directory: C:/Users/felip/Desktop/CodeProjects/Lunar-Mining/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/felip/Desktop/CodeProjects/Lunar-Mining/build/lunar_mining_tests[1]_include.cmake")
